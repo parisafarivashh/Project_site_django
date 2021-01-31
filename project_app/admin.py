859237dd8b9f0e1f11e7f33ff00285066af205e5
parisa <parisafarivash@gmail.com>
2021-01-31 08:37:49 +0330

    compelete admin.py

--- project_app/admin.py
+++ project_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, Product, Category, Profile, ItemOrder
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(ItemOrder)
+
